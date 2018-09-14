9c4197ee2ccfe3cc9c3fe4615a321982804da831
Jeun Yun Tsang <paul.tsang89@gmail.com>
2018-09-14 18:47:18 +0900

    start client side

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://auth:auth123@ds255262.mlab.com:55262/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
